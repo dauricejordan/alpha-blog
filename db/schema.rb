b978f9e976f10f2fd0e789bd8724137db30ba092
Alpha Blog <dauricejordan@hotmail.com>
2018-09-26 03:23:12 -0400

    Removed and Added all columns to users to fix production

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180925185053) do
+ActiveRecord::Schema.define(version: 20180926071135) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
